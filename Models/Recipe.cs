72f89110e7209a74edce34b0aea58bd4a8ee919e
AndileDimba <mlamuliforex@gmail.com>
2025-10-01 21:06:26 +0200

    project setup

---
+++ Models/Recipe.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipeShare.Api.Models
+{
+    public class Recipe
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; } = string.Empty;
+
+        [Required]
+        public string Ingredients { get; set; } = string.Empty;
+
+        [Required]
+        public string Steps { get; set; } = string.Empty;
+
+        [Range(1, int.MaxValue)]
+        public int CookingTimeMinutes { get; set; }
+        public string? DietaryTags { get; set; }
+    }
+}
